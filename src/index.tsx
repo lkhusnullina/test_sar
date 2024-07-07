d9ef116ed550187199a1c728e8dc2b7b7a3ead27
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-07-08 00:33:11 +0500

    ghpages router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,15 +2,17 @@ import { CardPage } from 'pages/CardPage/CardPage';
 import { MainPage } from 'pages/MainPage/MainPage';
 import { NotFoundPage } from 'pages/NotFoundPage/NotFoundPage';
 import { FunctionComponent } from 'react';
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 export const AppRoutes:FunctionComponent = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/card/:id" element={<CardPage />} />
-      <Route path="/404" element={<NotFoundPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/card/:id" element={<CardPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </HashRouter>
   );
 };
